b5216f56c155a7efed6717e0226115cc67b80ee7
Rabie Menad <rabie.menad07@outlook.com>
2022-08-19 20:53:52 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import "./index.scss";
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
