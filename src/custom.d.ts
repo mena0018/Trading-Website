18f3bc92c8179b8b05fe22b439c5172b9052ff6f
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-08-22 16:27:54 +0200

    Implementation of the Home Page

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,3 +1,4 @@
 declare module '*.webp';
 declare module '*.svg';
 declare module '*.scss';
+declare module '*.mp4';
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,33 @@
 import styles from "./Home.module.scss";
+import BackgroundVideo from "../../assets/video.mp4";
 
 
-export default function Home() {
+const Home = () => {
+  
   return (
-    <div className={styles.home_container}>
-      Home
+    <div className={styles.home}>
+
+      <video autoPlay loop muted>
+        <source src={BackgroundVideo} type='video/mp4' /> 
+      </video>
+
+      <div className={styles.home_header}>
+        <h1>Decentralized</h1>
+        <h1><span>Trading </span>Protocol</h1>
+        <p>Guaranteed liquidity trading for millions of users and top Ethereum applications.</p>
+
+        <div className={styles.btn_group}>
+          <button style={{ margin:"1rem" }} className="btn">Use Defi</button>
+          <button style={{ margin:"1rem" }} className="btn btn-outline">FAQ</button>
+        </div>
+      </div>
+
+      <div className={styles.home_bottom}>
+        <h2>Total Volume Secured : $45,561,625.00</h2>
+      </div>
+
     </div>
   )
 }
+
+export default Home
