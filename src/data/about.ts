2c9eaff477d4edf728b255313d27f7cf80f44216
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-08-24 16:28:43 +0200

    Diminution of the sentence length

--- src/data/about.ts
+++ src/data/about.ts
@@ -10,21 +10,21 @@ export const data: AboutCardType[] = [
   {
     icon: BlockChain,
     heading: 'Reliable, tamper-proof network',
-    text: 'Use decentralized technology to build a tamper-proof network that is secure and reliable. To make sure that your data is safe and secure, we use a blockchain that is built on top of a tamper-proof network.',
+    text: 'Use decentralized technology to build a tamper-proof network that is secure and reliable. To make sure that your data is safe and secure.',
   },
   {
     icon: Strapi ,
     heading: 'Seamless connection to any API',
-    text: 'Build on an open source platform, with a focus on security and scalability. We are building the next generation of blockchain technology That will help you build a tamper-proof network.',
+    text: 'Build on an open source platform, with a focus on security and scalability. We are building the next generation of blockchain technology.',
   },
   {
     icon: Secure ,
     heading: 'Proven, ready-made solutions',
-    text: 'We have built a number of blockchain solutions that are ready to use. We are building the next generation of blockchain technology. To make sure that your data is safe and secure.',
+    text: 'We have built a number of blockchain solutions that are ready to use. We are building the next generation of blockchain technology.',
   },
   {
     icon: ServerProcess ,
     heading: 'Secure off-chain computation',
-    text: 'Use a decentralized network to compute off-chain data. We are building the next generation of blockchain technology. To make sure that your data is safe and secure.',
+    text: 'Use a decentralized network to compute off-chain data. We are building the next generation of blockchain technology.',
   }
 ]
